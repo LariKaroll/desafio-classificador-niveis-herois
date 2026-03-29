1eed7272cc24ed639f3c69e23893dc7671800928
larikaroll <larikarol108@gmail.com>
2026-03-28 22:59:23 -0300

    refactor/desafio-completo

--- index.js
+++ index.js
@@ -1,10 +1,48 @@
 const prompt = require('readline-sync')
 
-let name = prompt.question('Nome do personagem: ')
-let xp = prompt.question('Digite o xp: ')
 let contador = 0
 
-while((name !== "") && (xp !== "") || contador){
-    console.log("nome: "+ name + "\nxp: " + xp)
-}
+do{
+    let name = prompt.question('Nome do personagem: ')
+    let xp = prompt.question('Digite o xp: ')
+    let nivel = ""
+    let condicaoDeEntrada = (name !== "") && (xp !== "")
+
+    if(condicaoDeEntrada === true){
+        contador++
+    }else{
+        break
+    }
+
+    if(xp <= 1000){
+        nivel = "Ferro"
+        console.log("O Herói de nome "+ name +" está no nível de " + nivel)
+    }else if((xp >= 1001)&&(xp <= 2000)){
+        nivel = "Bronze"
+        console.log("O Herói de nome "+ name +" está no nível de " + nivel)
+    }else if((xp >= 2001)&&(xp <= 5000)){
+        nivel = "Prata"
+        console.log("O Herói de nome "+ name +" está no nível de " + nivel)
+    }else if((xp >= 5001)&&(xp <= 7000)){
+        nivel = "Ouro"
+        console.log("O Herói de nome "+ name +" está no nível de " + nivel)
+    }else if((xp >= 7001)&&(xp <= 8000)){
+        nivel = "Platina"
+        console.log("O Herói de nome "+ name +" está no nível de " + nivel)
+    }else if((xp >= 8001)&&(xp <= 9000)){
+        nivel = "Ascendente"
+        console.log("O Herói de nome "+ name +" está no nível de " + nivel)
+    }else if((xp >= 9001)&&(xp <= 10000)){
+        nivel = "Imortal"
+        console.log("O Herói de nome "+ name +" está no nível de " + nivel)
+    }else{
+        nivel = "Radiante"
+        console.log("O Herói de nome "+ name +" está no nível de " + nivel)
+    }
+
+}while(contador >= 1)
+
+    
+
+
 
